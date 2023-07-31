7166ceb1d196dc5218868d72847b7f95b35ea39c
Slane-Holloway <julian.slane-holloway@pnnl.gov>
2023-07-31 13:45:36 -0700

    Added comments

--- src/app/app.py
+++ src/app/app.py
@@ -26,16 +26,13 @@ from src.api.workflow import Workflow
 import warnings
 import sys
 
-with open("dependencies.json") as file:
-    data = json.load(file)
-
 
 class GUI(QMainWindow):
     def __init__(self, setting):
         """QMainWindow to contain MetaForm, ImportForm, and Workflow Diagram.
 
         Args:
-            setting: (optional)
+            setting: (optional but should be sys.argv)
         """
         super().__init__()
         self.initialize_ui(setting)
@@ -47,16 +44,16 @@ class GUI(QMainWindow):
         self.import_form = ImportForm()
         self.states_form = WorkflowDiagram(setting)
 
+        # list containing meta, imports, and state for display on LHS
         self.column_list = QListWidget()
         self.column_list.addItems(["Meta", "Imports", "State"])
-        # self.column_list.setStyleSheet("QListWidget::item { text-align: center; }")
         self.column_list.currentItemChanged.connect(self.display_form)
         self.column_list.setStyleSheet(
             "QListWidget::item:selected { background-color: #145369; }"
         )
         self.column_list.setCurrentItem(self.column_list.item(0))
-        # self.column_list.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 
+        # make and reposition frame containing meta, imports, and state
         self.column_frame = QFrame()
         self.column_frame.setFrameStyle(QFrame.Shape.NoFrame)
         self.column_frame.setFixedWidth(100)
@@ -72,12 +69,14 @@ class GUI(QMainWindow):
         self.column_frame_container.addWidget(self.column_frame)
         self.column_frame_container.setAlignment(Qt.AlignmentFlag.AlignTop)
 
+        # layouts that are displayed when meta, imports, or state is selected
         middle_layout = QHBoxLayout()
         middle_layout.addLayout(self.column_frame_container)
         middle_layout.addWidget(self.meta_form)
         middle_layout.addWidget(self.import_form)
         middle_layout.addWidget(self.states_form)
 
+        # validate and submit buttons
         self.validate_button = QPushButton("Validate")
         self.validate_button.setFixedSize(100, 20)
         self.validate_button.clicked.connect(self.validate_form)
@@ -87,11 +86,13 @@ class GUI(QMainWindow):
         self.submit_button.setFixedSize(100, 20)
         self.submit_button.clicked.connect(self.submit_form)
 
+        # group validate and submit buttons
         buttons = QHBoxLayout()
         buttons.addWidget(self.validate_button)
         buttons.addWidget(self.submit_button)
         buttons.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
+        # set layout for entire window
         main_layout = QVBoxLayout()
         main_layout.addLayout(middle_layout)
         main_layout.addLayout(buttons)
@@ -103,6 +104,7 @@ class GUI(QMainWindow):
         self.initialize_toolbar()
 
     def initialize_toolbar(self):
+        """Creates toolbar to give option for importing .json and exporting .json"""
         toolbar = QToolBar()
         self.addToolBar(toolbar)
 
@@ -119,6 +121,7 @@ class GUI(QMainWindow):
         toolbar.addAction(file_menu.menuAction())
 
     def exportFile(self):
+        """Exports current state as a .json to local storage"""
         fp, _ = QFileDialog.getSaveFileName(
             self, "Save JSON File", "", "JSON Files (*.json);;All Files (*)"
         )
@@ -132,6 +135,7 @@ class GUI(QMainWindow):
                 print("error")
 
     def importFile(self):
+        """Imports a .json file to use a state and loads file into the GUI"""
         file_dialog = QFileDialog()
         file_dialog.setWindowTitle("Select a JSON File")
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
@@ -141,6 +145,7 @@ class GUI(QMainWindow):
             with open(file_path, "r") as f:
                 workflow = json.load(f)
                 if isinstance(workflow, dict):
+                    # have each tab fill in the data
                     self.meta_form.read_import(
                         workflow.get("workflow_name"), workflow.get("meta")
                     )
@@ -148,9 +153,15 @@ class GUI(QMainWindow):
                     self.states_form.read_import(workflow.get("states"))
                     self.get_workflow()
                 else:
+                    # error if selected file cannot be converted to a dict
                     print("error")
 
     def display_form(self, current_item):
+        """Displays tab that is selected
+
+        Args:
+            current_item (str): Name of tab (can be 'Meta', 'Imports', or 'State')
+        """
         if current_item.text() == self.column_list.item(0).text():
             self.meta_form.show()
             self.import_form.hide()
@@ -164,14 +175,25 @@ class GUI(QMainWindow):
             self.import_form.hide()
             self.states_form.show()
 
-    def create_json(self, workflow):
+    def create_json(self, states):
+        """Collects information from all tabs in order to build a final .json
+
+        Args:
+            states (dict): The list of current states
+
+        Returns:
+            dict: The finalized .json file
+        """
         data = {
             "workflow_name": self.meta_form.get_workflow_name(),
             "meta": self.meta_form.get_meta(),
             "imports": self.import_form.get_imports(),
             "states": {},
         }
-        for item in workflow:
+
+        # Since each state contains its own name, and isn't a key in data['states'][name], we must make it so it
+        # is a key to this dict
+        for item in states:
             copy_item = dict(item)
             title = copy_item.pop("Title")
             data["states"][title] = copy_item
@@ -179,20 +201,35 @@ class GUI(QMainWindow):
         return json_data
 
     def submit_form(self):
-        workflow_path = self.get_workflow()
-        json_data = self.create_json(workflow_path)
+        """Workflow for submitting the state. Triggered on the click of the Submit button. Displays a popup which
+        shows the progress of running the state.
+        """
+        states = self.get_workflow()
+        json_data = self.create_json(states)
+
         popup = SubmitPopup()
+
         worker = Worker(json_data)
         worker.update_text.connect(popup.update_text)
+
         worker.start()
         popup.exec()
 
     def get_workflow(self):
+        """Organizes the states into a single list, colors states based on placing, returns organized workflow
+
+        Returns:
+            list: A compiled version of the state dicts in DFS order
+        """
+
+        # find all CustomItems in the scene
         items = [
             item
             for item in self.states_form.scene.items()
             if isinstance(item, CustomItem)
         ]
+
+        # prepare for DFS
         roots = []
         for i in items:
             parent = True
@@ -202,10 +239,11 @@ class GUI(QMainWindow):
                     break
             if parent:
                 roots.append(i)
-
                 visited = set()
+
         paths = []
 
+        # DFS helper method
         def dfs_helper(item, path):
             path.append(item)
             visited.add(item)
@@ -235,6 +273,8 @@ class GUI(QMainWindow):
 
         workflow_path = []
         visited = set()
+
+        # create final path through workflow
         for path in paths:
             for node in path:
                 if node not in visited:
@@ -243,10 +283,15 @@ class GUI(QMainWindow):
         return workflow_path
 
     def validate_form(self):
+        """Workflow for validating the state. Triggered on the click of the Validate button. Enables the submit
+        button if the workflow is validated.
+        """
         workflow_path = self.get_workflow()
         json_data = self.create_json(workflow_path)
+
         warnings.simplefilter(action="ignore", category=FutureWarning)
         warnings.simplefilter(action="ignore", category=ResourceWarning)
+
         validate_wf = Workflow(json_data)
         valid = validate_wf.validate(verbose=True)
         if valid:
@@ -255,14 +300,21 @@ class GUI(QMainWindow):
 
 class UserSetting(QDialog):
     def __init__(self):
+        """QDialog to ask whether user wants Basic or Advanced settings in creating the workflow. Users
+        are still able to add basic or advanced popups, but this diagram changes what is displayed on edit.
+        Some rethinking about it may be necessary. Runs the GUI based on the setting chosen
+        """
         super().__init__()
         self.setWindowTitle("ConStrain")
         query = QLabel("Advanced or basic user settings?")
+
+        # buttons
         self.advanced_button = QPushButton("Advanced")
         self.advanced_button.clicked.connect(self.showAdvanced)
         self.basic_button = QPushButton("Basic")
         self.basic_button.clicked.connect(self.showBasic)
 
+        # add buttons and query to layout
         form_layout = QVBoxLayout()
         button_layout = QHBoxLayout()
 
@@ -275,11 +327,13 @@ class UserSetting(QDialog):
         self.setLayout(form_layout)
 
     def showBasic(self):
+        """Displays basic GUI on selection"""
         self.close()
         self.gui = GUI("basic")
         self.gui.show()
 
     def showAdvanced(self):
+        """Displays advanced GUI on selection"""
         self.close()
         self.gui = GUI("advanced")
         self.gui.show()
@@ -289,14 +343,24 @@ class Worker(QThread):
     update_text = pyqtSignal(str)
 
     def __init__(self, json_data):
+        """Given the finalized workflow, creates a thread that runs the workflow in the Workflow API
+
+        Args:
+            json_data (dict): The finalized, json formatted dict of information from each tab
+        """
         super(Worker, self).__init__()
         self.json_data = json_data
 
     def run(self):
+        """Runs the thread"""
+
+        # captures sys.stdout in an EmittingStream object to display in a popup
         sys.stdout = EmittingStream(self.update_text)
 
         warnings.simplefilter(action="ignore", category=FutureWarning)
         warnings.simplefilter(action="ignore", category=ResourceWarning)
+
+        # creates and runs the workflow based on the workflow provided
         wf = Workflow(self.json_data)
         wf.run_workflow(verbose=True)
 
@@ -306,7 +370,7 @@ class EmittingStream:
         self._signal = signal
 
     def write(self, message):
-        # Emit the signal with the message to update the QTextEdit
+        # emits the signal with the message to update the QTextEdit
         self._signal.emit(message.strip())
 
     def flush(self):
@@ -315,6 +379,7 @@ class EmittingStream:
 
 class SubmitPopup(QDialog):
     def __init__(self):
+        """Creates a read-only popup containing the verbose run through Workflow"""
         super(SubmitPopup, self).__init__()
         self.init_ui()
 
