d44100d02e9eaff9601f9f2f04fd522202509770
FanFeng <fengfan6696@gmail.com>
2024-03-22 11:58:34 -0700

    add unittest if path does not exist

--- tests/api/test_flexible_calling.py
+++ tests/api/test_flexible_calling.py
@@ -12,16 +12,12 @@ class TestFlexibleCalling(unittest.TestCase):
     def test_no_dir_provided(self):
         """This test checks when no working directory is provided,
         if the program will behave correctly"""
+        
         with self.assertLogs() as logobs:
-            json_case_path = (
-                "./data/verification_case_unit_test/verification_case_unit_test_NotAString.json"
-            )
+            json_case_path = "./data/verification_case_unit_test/verification_case_unit_test_NotAString.json"
             workflow = Workflow(workflow=json_case_path)    
             self.assertEqual(logobs.output[0],"ERROR:root:working directory specified is not a valid string.")
-            
-            
-
-
+                    
     def test_invalid_dir_string(self):
         """This test checks when a invalid dir string is provided,
         if the program will behave correctly"""
@@ -33,8 +29,10 @@ class TestFlexibleCalling(unittest.TestCase):
         """This test checks when a valid wd is provided but it doesn't exist,
         if the program will behave correctly"""
 
-        a = 1
-        self.assertEqual(1,a)
+        with self.assertLogs() as logobs:
+            json_case_path = "./data/verification_case_unit_test/verification_case_unit_test_NotExist.json"
+            workflow = Workflow(workflow=json_case_path)    
+            self.assertEqual(logobs.output[0],"ERROR:root:working directory specified does not exist.")              
 
     def test_valid_dir(self):
         """This test checks when a working directory is provided and it also points to the correct path,
