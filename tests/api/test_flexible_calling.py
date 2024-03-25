0c4bc3cf8edcd6b915cc45b106716e08c7383276
FanFeng <fengfan6696@gmail.com>
2024-03-25 01:00:30 -0700

    test unittest

--- tests/api/test_flexible_calling.py
+++ tests/api/test_flexible_calling.py
@@ -48,49 +48,7 @@ class TestFlexibleCalling(unittest.TestCase):
                 "ERROR:root:working directory specified is not a valid string.",
             )
 
-    def test_Linux_path(self):
-        """This test check if the program can detect the working path provided is in Linux format."""
-        with self.assertLogs() as logobs:
-            json_case_path = "./tests/api/data/verification_case_unit_test/verification_case_unit_test_ValidPath.json"
 
-            # Change working_dir value in the json file to a valid path in Linux format
-            with open(json_case_path, "r") as f:
-                workflow_dict = json.load(f)
-                workflow_dict["working_dir"] = "./tests/api/result"
-
-            with open(json_case_path, "w") as f:
-                json.dump(workflow_dict, f)
-
-            workflow = Workflow(workflow=json_case_path)
-            # change current working directory back
-            os.chdir("../../..")
-
-            self.assertEqual(
-                logobs.output[0],
-                "INFO:root:the working dir provided is in Linux format.",
-            )
-
-    def test_Win_path(self):
-        """This test check if the program can detect the working path provided is in WIN format."""
-        with self.assertLogs() as logobs:
-            json_case_path = "./tests/api/data/verification_case_unit_test/verification_case_unit_test_ValidPath.json"
-
-            # Change working_dir value in the json file to a valid path in Win format
-            with open(json_case_path, "r") as f:
-                workflow_dict = json.load(f)
-                workflow_dict["working_dir"] = ".\\tests\\api\\result"
-
-            with open(json_case_path, "w") as f:
-                json.dump(workflow_dict, f)
-
-            workflow = Workflow(workflow=json_case_path)
-            # change current working directory back
-            os.chdir("../../..")
-
-            self.assertEqual(
-                logobs.output[0],
-                "INFO:root:the working dir provided is in Win format.",
-            )
 
     def test_dir_not_exist(self):
         """This test checks when a valid wd is provided but it doesn't exist,
@@ -112,47 +70,6 @@ class TestFlexibleCalling(unittest.TestCase):
                 "ERROR:root:working directory specified does not exist.",
             )
 
-    def test_valid_dir(self):
-        """This test checks when a working directory is provided and it also points to the correct path,
-        if the program will behave correctly"""
-        with self.assertLogs() as logobs:
-            json_case_path = "./tests/api/data/verification_case_unit_test/verification_case_unit_test_ValidPath.json"
-
-            # Change working_dir value in the json file to a valid path
-            with open(json_case_path, "r") as f:
-                workflow_dict = json.load(f)
-                workflow_dict["working_dir"] = "./tests/api/result"
-
-            with open(json_case_path, "w") as f:
-                json.dump(workflow_dict, f)
-
-            workflow = Workflow(workflow=json_case_path)
-
-            # change current working directory back
-            os.chdir("../../..")
-
-            self.assertEqual(
-                logobs.output[1],
-                "INFO:root:Change current working path to the specified path.",
-            )
-
-            # Change working_dir value in the json file to a valid path in Win format
-            with open(json_case_path, "r") as f:
-                workflow_dict = json.load(f)
-                workflow_dict["working_dir"] = ".\\tests\\api\\result"
-
-            with open(json_case_path, "w") as f:
-                json.dump(workflow_dict, f)
-
-            workflow = Workflow(workflow=json_case_path)
-
-            # change current working directory back
-            os.chdir("../../..")
-
-            self.assertEqual(
-                logobs.output[1],
-                "INFO:root:Change current working path to the specified path.",
-            )
 
     """
     1. Probably another test is needed to check if the program can successfully detect if the WD provided is Linux format or Window format. 
