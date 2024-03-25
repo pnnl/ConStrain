321e939fd44b5d0c4de2259e46c4fa9e67f533a8
FanFeng <fengfan6696@gmail.com>
2024-03-25 00:09:30 -0700

    fix unittest

--- tests/api/test_flexible_calling.py
+++ tests/api/test_flexible_calling.py
@@ -63,7 +63,7 @@ class TestFlexibleCalling(unittest.TestCase):
 
             workflow = Workflow(workflow=json_case_path)
             # change current working directory back
-            os.chdir("..")
+            os.chdir("../../..")
 
             self.assertEqual(
                 logobs.output[0],
@@ -85,7 +85,7 @@ class TestFlexibleCalling(unittest.TestCase):
 
             workflow = Workflow(workflow=json_case_path)
             # change current working directory back
-            os.chdir("..")
+            os.chdir("../../..")
 
             self.assertEqual(
                 logobs.output[0],
@@ -129,7 +129,7 @@ class TestFlexibleCalling(unittest.TestCase):
             workflow = Workflow(workflow=json_case_path)
 
             # change current working directory back
-            os.chdir("..")
+            os.chdir("../../..")
 
             self.assertEqual(
                 logobs.output[1],
@@ -139,7 +139,7 @@ class TestFlexibleCalling(unittest.TestCase):
             # Change working_dir value in the json file to a valid path in Win format
             with open(json_case_path, "r") as f:
                 workflow_dict = json.load(f)
-                workflow_dict["working_dir"] = ".\\testsi\\result"
+                workflow_dict["working_dir"] = ".\\tests\\api\\result"
 
             with open(json_case_path, "w") as f:
                 json.dump(workflow_dict, f)
@@ -147,7 +147,7 @@ class TestFlexibleCalling(unittest.TestCase):
             workflow = Workflow(workflow=json_case_path)
 
             # change current working directory back
-            os.chdir("..")
+            os.chdir("../../..")
 
             self.assertEqual(
                 logobs.output[1],
