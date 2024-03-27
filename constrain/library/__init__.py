89322be3d918e6acd48049da15345aec8c5a92e6
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2024-03-27 13:29:33 -0700

    Add verification for Appendix G system fan operation requirement.

--- constrain/library/__init__.py
+++ constrain/library/__init__.py
@@ -36,6 +36,7 @@ from .LocalLoopSaturationDirectActingMax import *
 from .LocalLoopSaturationDirectActingMin import *
 from .LocalLoopSaturationReverseActingMax import *
 from .LocalLoopSaturationReverseActingMin import *
+from .AppendixGHVACSystemFanOperation import *
 
 __all__ = [
     "AutomaticOADamperControl",
@@ -80,4 +81,5 @@ __all__ = [
     "LocalLoopSaturationReverseActingMax",
     "LocalLoopSaturationReverseActingMin",
     # "LocalLoopHuntingActivation",
+    "AppendixGHVACSystemFanOperation",
 ]
