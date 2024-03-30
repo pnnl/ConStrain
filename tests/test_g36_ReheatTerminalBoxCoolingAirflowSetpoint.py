4dac2eec8393737174dcab8c70575b7799b2d36a
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2024-03-30 11:30:19 -0700

    clean comment

--- tests/test_g36_ReheatTerminalBoxCoolingAirflowSetpoint.py
+++ tests/test_g36_ReheatTerminalBoxCoolingAirflowSetpoint.py
@@ -10,8 +10,6 @@ import numpy as np
 
 class TestG36ReheatTerminalBoxCoolingAirflowSetpoint(unittest.TestCase):
     def test_g36_reheat_terminal_box_cooling_airflow_setpoint(self):
-        # points = ["operation_mode", "zone_state", "v_cool_max", "v_min", "v_spt"]
-
         points = [
             "operation_mode",
             "zone_state",
