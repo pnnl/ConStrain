abf441bfc63ab4b813fa3974949f11bb26bf23b1
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2024-04-04 10:55:55 -0700

    Add docstrings.

--- constrain/library/AppendixGHVACSystemFanOperation.py
+++ constrain/library/AppendixGHVACSystemFanOperation.py
@@ -1,3 +1,62 @@
+"""
+ASHRAE 90.1-2022 
+### Description
+
+Section G3.1.4 HVAC System Fan Schedules
+
+- Schedules for HVAC system fans that provide outdoor air for ventilation shall run continuously whenever spaces are occupied and shall be cycled ON and OFF to meet heating and cooling loads during unoccupied hours.
+
+Note: exceptions to this requirement are not capture since they depend on system design which is not related to system control.
+
+Verification Item:
+
+- Check that when a zone is occupied and served by a system that provides outdoor air the system runs continuously and cycles when the zone is occupied.
+
+### Verification logic
+
+In the following pseudo code `None` means that the verification is not verifiable, `True` means that it is verifiable and that the verification passes, and `False` that it fails.
+
+```
+# Check that the system provide OA
+# This is a one-time check, do not perform further checks if None is returned
+If sum(m_oa) == 0
+  return None
+Endif
+
+# This assume that the system does provide OA to the space as per the first check
+potential_failures_count = 0
+potential_pass_count = 0
+If o
+  If fan_runtime_fraction == 1
+    return True
+  Else
+    return False
+  Endif
+Else
+  If fan_runtime_fraction == 1 # the system could be "cycling" for the whole timestep so add to counter
+    potential_failures_count += 1
+    return None
+  Else
+    potential_pass_count += 1
+    return None
+  Endif
+Endif
+
+# Check that if the system has been cycling for a whole timestep it also cycles during some
+# If it hasn't we assume that the system is set to run continuously when the zone is unoccupied
+# This is a one-time check, it can be used to make a final pass/fail decision
+If potential_failures_count > 0 and potential_pass_count == 0
+  return False
+Endif
+```
+
+### Data requirements
+- o: number of occupants sensed in the zones served by the system.
+- fan_runtime_fraction: system fan runtime fraction (between 0 and 1).
+- m_oa: system outdoor air flow rate.
+- tol_o: occupancy threshold; below that value the zones are considered unoccupied.
+
+"""
 from constrain.checklib import RuleCheckBase
 
 
--- constrain/library/InteriorLightingControlAutomaticFullOff.py
+++ constrain/library/InteriorLightingControlAutomaticFullOff.py
@@ -1,3 +1,46 @@
+"""
+ASHRAE 90.1-2022 
+### Description
+
+Section 9.4.1.1.h Automatic full OFF control
+
+- All lighting in the space, including lighting connected to emergency circuits,shall be automatically shut off within 20 minutes of all occupants leaving the space. A control device meeting this requirement shall control no more than 5000 ft2.
+- Exceptions:
+  - The following lighting is not required to be automatically shut off:
+    1. Lighting required for 24/7 continuous operation.
+    2. Lighting in spaces where patient care is rendered.
+    3. General lighting and task lighting in spaces where automatic shutoff would endanger the safety or security of the room or building occupants.
+    4. Lighting load not exceeding 0.02 W/ft2 multiplied by the gross lighted floor area of the building.
+
+Verification Item:
+
+- Check if the lighting power is reduced when no occupancy is detected.
+
+### Verification logic
+
+```
+If lighted_floor_area >= 5000:
+    return False
+
+date_diff = current_date - last_reported_occupancy # in min
+If o < tol_o and date_diff > 20
+    If total_lighting_power / lighted_floor_area <= 0.02
+        Pass
+    Else
+        Fail
+    Endif
+Else
+    Untested
+Endif
+```
+
+### Data requirements
+- o: number of occupants sensed in the zones served by the system.
+- total_lighting_power: reported total lighting power (not the design total lighting power)
+- lighted_floor_area: area lit by the device/system considered
+- tol_o: occupancy threshold; below that value the zones are considered unoccupied.
+
+"""
 from constrain.checklib import RuleCheckBase
 import numpy as np
 
