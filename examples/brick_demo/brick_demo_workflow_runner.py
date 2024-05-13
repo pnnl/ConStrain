8cdc8ef8c0b257dde64c4d9db40c082bf983a07c
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2024-05-13 14:18:58 -0700

    Move brick demo.

--- examples/brick_demo/brick_demo_workflow_runner.py
+++ examples/brick_demo/brick_demo_workflow_runner.py
@@ -1,11 +1,11 @@
 import sys
 import warnings
 
-sys.path.append("./src")
+sys.path.append("./constrain")
 
-from api import Workflow
+from constrain.api import Workflow
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.simplefilter(action="ignore", category=ResourceWarning)
-workflow = Workflow(workflow="./demo/brick_demo/brick_workflow.json")
+workflow = Workflow(workflow="./examples/brick_demo/brick_workflow.json")
 workflow.run_workflow(verbose=True)
