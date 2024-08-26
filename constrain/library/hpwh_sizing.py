46bcbdd6ab225c23afa917f866278934798a0882
yunjoonjung <yunjoon.jung@pnnl.gov>
2024-08-26 14:21:37 -0700

    Updated HPWH_sizing logic, Added workflow testing

--- constrain/library/hpwh_sizing.py
+++ constrain/library/hpwh_sizing.py
@@ -1,4 +1,7 @@
 from constrain.checklib import RuleCheckBase
+import numpy as np
+
+TEMP_TOLERANCE = 0.5  # deg C
 
 
 class HPWH_sizing(RuleCheckBase):
@@ -8,24 +11,36 @@ class HPWH_sizing(RuleCheckBase):
         "HeatingRate_waterheater1",
         "HeatingRate_waterheater2",
         "T_amb_parameter",
+        "HPWH_output_target_percent",
     ]
 
-    def hpwh_load(self, data):
-        total_hpwh_load = (
-            data["HeatingRate_dx_coil"]
-            + data["HeatingRate_waterheater1"]
-            + data["HeatingRate_waterheater2"]
+    def verify(self):
+        self.df["total_hpwh_load"] = (
+            self.df["HeatingRate_dx_coil"]
+            + self.df["HeatingRate_waterheater1"]
+            + self.df["HeatingRate_waterheater2"]
         )
 
-        if data["T_amb"] <= data["T_amb_parameter"] or total_hpwh_load == 0.0:
-            return "untested"
-        elif data["HeatingRate_dx_coil"] / total_hpwh_load >= 1:
-            return True
-        else:
-            return False
+        self.df["HPWH_output"] = np.where(
+            self.df["total_hpwh_load"] != 0.0,
+            self.df["HeatingRate_dx_coil"] / self.df["total_hpwh_load"] * 100,
+            0.0,
+        )
 
-    def verify(self):
-        self.result = self.df.apply(lambda d: self.hpwh_load(d), axis=1)
+        min_hpwh_output = self.df[
+            (self.df["T_amb"] > self.df["T_amb_parameter"])
+            & (self.df["HPWH_output"] > 0.0)
+        ]["HPWH_output"].min()
+        min_hpwh_output = 0.0 if min_hpwh_output is np.nan else min_hpwh_output
+
+        HPWH_output_target_percent = self.df["HPWH_output_target_percent"].iloc[0]
+
+        if min_hpwh_output >= HPWH_output_target_percent:
+            self.df["result"] = True
+        elif min_hpwh_output < HPWH_output_target_percent:
+            self.df["result"] = False
+
+        self.result = self.df["result"]
 
     def check_bool(self):
         if len(self.result[self.result == False] > 0):
--- tests/test_HPWH_sizing.py
+++ tests/test_HPWH_sizing.py
@@ -2,45 +2,148 @@ import sys
 import unittest
 
 sys.path.append("./constrain")
+import datetime
+
 import pandas as pd
 from lib_unit_test_runner import *
 
 
 class TestHPWHSizing(unittest.TestCase):
-    def test_HPWH_sizing(self):
+    def test_HPWH_sizing_pass(self):
         points = [
             "T_amb",
             "HeatingRate_dx_coil",
             "HeatingRate_waterheater1",
             "HeatingRate_waterheater2",
             "T_amb_parameter",
+            "HPWH_output_target_percent",
+        ]
+
+        timestamp = [
+            datetime(2024, 1, 1, 12, 0, 0),
+            datetime(2024, 1, 1, 13, 0, 0),
+            datetime(2024, 1, 1, 14, 0, 0),
+            datetime(2024, 1, 1, 15, 0, 0),
         ]
 
         data = [
-            [3.0, 0, 0, 0, 3.7],  # untested - when T_amb <= T_amb_parameter,
-            [5, 0, 0, 0, 3.7],  # untested - when total_hpwh_load == 0
-            [
-                5.0,
-                30,
-                0,
-                0,
-                3.7,
-            ],  # pass - when `HeatingRate_dx_coil` can meet all the load
-            [
-                5.0,
-                30,
-                0,
-                20,
-                3.7,
-            ],  # fail - when `HeatingRate_dx_coil` cannot meet all the load
+            [3.0, 50, 0, 50, 2.7, 50],
+            [3.0, 60, 0, 60, 2.7, 50],
+            [5.0, 60, 10, 30, 2.7, 50],
+            [5.0, 80, 40, 40, 2.7, 50],
         ]
 
-        df = pd.DataFrame(data, columns=points)
+        df = pd.DataFrame(data, columns=points, index=timestamp)
 
         verification_obj = run_test_verification_with_data("HPWH_sizing", df)
 
         results = pd.Series(list(verification_obj.result))
-        expected_results = pd.Series(["untested", "untested", True, False])
+        expected_results = pd.Series(
+            [
+                True,
+                True,
+                True,
+                True,
+            ]
+        )
+        self.assertTrue(results.equals(expected_results))
+
+        binary_result = verification_obj.check_bool()
+        self.assertTrue(binary_result)
+
+    def test_HPWH_sizing_fail(self):
+        points = [
+            "T_amb",
+            "HeatingRate_dx_coil",
+            "HeatingRate_waterheater1",
+            "HeatingRate_waterheater2",
+            "T_amb_parameter",
+            "HPWH_output_target_percent",
+        ]
+
+        timestamp = [
+            datetime(2024, 1, 1, 12, 0, 0),
+            datetime(2024, 1, 1, 13, 0, 0),
+            datetime(2024, 1, 1, 14, 0, 0),
+            datetime(2024, 1, 1, 15, 0, 0),
+        ]
+
+        data = [
+            [3.0, 50, 0, 50, 2.7, 50],
+            [3.0, 60, 0, 60, 2.7, 50],
+            [5.0, 60, 10, 30, 2.7, 50],
+            [
+                5.0,
+                30,
+                40,
+                40,
+                2.7,
+                50,
+            ],  # fail because of this line (e.g., 30 / (30+40+40) < 50)
+        ]
+
+        df = pd.DataFrame(data, columns=points, index=timestamp)
+
+        verification_obj = run_test_verification_with_data("HPWH_sizing", df)
+
+        results = pd.Series(list(verification_obj.result))
+        expected_results = pd.Series(
+            [
+                False,
+                False,
+                False,
+                False,
+            ]
+        )
+        self.assertTrue(results.equals(expected_results))
+
+        binary_result = verification_obj.check_bool()
+        self.assertFalse(binary_result)
+
+    def test_HPWH_sizing_fail_all_temps_less_than_temp_param(self):
+        points = [
+            "T_amb",
+            "HeatingRate_dx_coil",
+            "HeatingRate_waterheater1",
+            "HeatingRate_waterheater2",
+            "T_amb_parameter",
+            "HPWH_output_target_percent",
+        ]
+
+        timestamp = [
+            datetime(2024, 1, 1, 12, 0, 0),
+            datetime(2024, 1, 1, 13, 0, 0),
+            datetime(2024, 1, 1, 14, 0, 0),
+            datetime(2024, 1, 1, 15, 0, 0),
+        ]
+
+        data = [
+            [1.0, 50, 0, 50, 2.7, 50],
+            [1.0, 60, 0, 60, 2.7, 50],
+            [2.0, 60, 10, 30, 2.7, 50],
+            [
+                2.0,
+                30,
+                40,
+                40,
+                2.7,
+                50,
+            ],
+        ]
+
+        df = pd.DataFrame(data, columns=points, index=timestamp)
+
+        verification_obj = run_test_verification_with_data("HPWH_sizing", df)
+
+        results = pd.Series(list(verification_obj.result))
+        expected_results = pd.Series(
+            [
+                False,
+                False,
+                False,
+                False,
+            ]
+        )
         self.assertTrue(results.equals(expected_results))
 
         binary_result = verification_obj.check_bool()
