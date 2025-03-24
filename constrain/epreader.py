94fcafa55d98383c07c5ed646421166f9b6840f5
Lerond <jeremy.lerond@pnnl.gov>
2025-03-24 15:55:08 -0700

    Black

--- constrain/epreader.py
+++ constrain/epreader.py
@@ -98,22 +98,22 @@ class CSVReader(EPReader):
             if len(current_picks) > 1:
                 maxratio = None
                 for i in range(len(current_picks)):
-                    current_ratio  = fuzz.ratio(
+                    current_ratio = fuzz.ratio(
                         current_picks[i].upper(), requestcol.upper()
                     )
                     reset = False
                     if maxratio is None:
                         reset = True
                     else:
-                        if current_ratio  > maxratio:
+                        if current_ratio > maxratio:
                             reset = True
-                        if current_ratio  == maxratio:
+                        if current_ratio == maxratio:
                             print(
                                 "ERROR: observe same fuzzywuzzy match ratio for two columns, investigation needed."
                             )
                             return None
                     if reset:
-                        maxratio = current_ratio 
+                        maxratio = current_ratio
                         picked_csvcol = current_picks[i]
             else:
                 picked_csvcol = current_picks[0]
--- constrain/library/G36ReheatTerminalBoxHeatingCoilLowerBound.py
+++ constrain/library/G36ReheatTerminalBoxHeatingCoilLowerBound.py
@@ -45,7 +45,10 @@ class G36ReheatTerminalBoxHeatingCoilLowerBound(RuleCheckBase):
         if dat >= (10 - self.get_tolerance("temperature", "discharge_air")):
             return True
         else:
-            if heating_coil_command < 100 - self.get_tolerance("damper", "command") * 100:
+            if (
+                heating_coil_command
+                < 100 - self.get_tolerance("damper", "command") * 100
+            ):
                 return False
             else:
                 return True  # heating coil tried its best
