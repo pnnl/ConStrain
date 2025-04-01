91e6bacac0f8405b496bae97ac1593ce996dc53a
Julian Slane <julian.slane-holloway@pnnl.gov>
2025-04-01 12:00:04 -0700

    Add build script

---
+++ build.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+
+def build_wizard_executable():
+    gui_path = "tools/wizard/main.py"
+    schema_dir = "schema"
+
+    command = [
+        "pyinstaller",
+        "--onefile",
+        "--windowed",
+        f"--add-data={os.path.join(schema_dir, 'verification_cases.schema.json')}:{schema_dir}",
+        f"--add-data={os.path.join(schema_dir, 'library.json')}:{schema_dir}",
+        gui_path,
+    ]
+
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    build_wizard_executable()
