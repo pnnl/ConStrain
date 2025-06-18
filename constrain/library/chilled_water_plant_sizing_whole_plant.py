c2acae69649c6c57f497833ac9d9aeea727564c9
lymereJ <jeremy.lerond@gmail.com>
2025-06-18 16:29:06 -0700

    Modifications to the ChilledWaterPlantSizingWholePlant verifications based on comments. Update unit tests.

--- constrain/library/chilled_water_plant_sizing_whole_plant.py
+++ constrain/library/chilled_water_plant_sizing_whole_plant.py
@@ -66,11 +66,11 @@ from constrain.checklib import RuleCheckBase
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
 
 
-Cp = 4.184  # kJ/(kg*K), specific heat
+Cp = 4184  # J/(kg*K)
 UPPER_TEMP_AIR_OUTDOOR_LIMIT = 48.89  # deg C ~= 120 deg F
-ACCEPTABLE_CHILLER_SIZING_RATIO = 0.95
 
 
 class ChilledWaterPlantSizingWholePlant(RuleCheckBase):
@@ -81,10 +81,24 @@ class ChilledWaterPlantSizingWholePlant(RuleCheckBase):
         "load_plant_water_chilled",
         "temperature_air_outdoor",
         "capacity_nominal_plant_water_chilled",
+        "ratio_sizing_plant_water_chilled_acceptable",
     ]
 
-    def verify(self):
+    def calc_temp_from_load(self, load, model):
+        a = model.coef_[2]
+        b = model.coef_[1]
+        c = model.intercept_ - load
 
+        discriminant = b**2 - 4 * a * c
+        if discriminant < 0:
+            raise ValueError("No real solution exists for the given y.")
+
+        x1 = (-b + np.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - np.sqrt(discriminant)) / (2 * a)
+
+        return max(x1, x2)
+
+    def verify(self):
         # Calculate `load_plant_water_chilled` if any value is None
         if self.df["load_plant_water_chilled"].isnull().any():
             self.df["load_plant_water_chilled"] = (
@@ -96,83 +110,36 @@ class ChilledWaterPlantSizingWholePlant(RuleCheckBase):
                 )
             )
 
-        # Perform linear, quadratic, cubic  regressions of `load_plant_water_chilled` = f(`temperature_air_outdoor`)
-        # 1. linear
-        linear_model = LinearRegression().fit(
-            self.df[["temperature_air_outdoor"]], self.df[["load_plant_water_chilled"]]
+        # Perform regressions of `load_plant_water_chilled` = f(`temperature_air_outdoor`)
+        feat = PolynomialFeatures(degree=2)
+        X = feat.fit_transform(self.df[["temperature_air_outdoor"]].values)
+        model = LinearRegression().fit(X, self.df["load_plant_water_chilled"])
+        y = model.predict(X)
+        coefficient_of_determination = r2_score(
+            self.df[["load_plant_water_chilled"]], y
         )
-
-        # 1.1 Make a prediction
-        y_pred_linear = linear_model.predict(self.df[["temperature_air_outdoor"]])
-
-        # 1.2 Calculate R^2 value
-        r_squared_linear = r2_score(
-            self.df[["load_plant_water_chilled"]], y_pred_linear
-        )
-
-        # 2. quadratic
-        # 2.1 Transform features to quadratic (second-degree)
-        quadratic_feature = PolynomialFeatures(degree=2)
-        x_quadratic = quadratic_feature.fit_transform(
-            self.df[["temperature_air_outdoor"]]
-        )
-
-        # 2.2 train the model
-        quadratic_model = LinearRegression().fit(
-            x_quadratic, self.df[["load_plant_water_chilled"]]
-        )
-
-        # 2.3 Make a prediction
-        y_quadratic_pred = quadratic_model.predict(x_quadratic)
-
-        # 2.4 Calculate R^2 value
-        r_squared_quadratic = r2_score(
-            self.df[["load_plant_water_chilled"]], y_quadratic_pred
-        )
-
-        # 3. cubic
-        # 3.1 Transform features to quadratic (third-degree)
-        cubic_feature = PolynomialFeatures(degree=3)
-        x_cubic = cubic_feature.fit_transform(self.df[["temperature_air_outdoor"]])
-
-        # 3.2 train the model
-        cubic_model = LinearRegression().fit(
-            x_cubic, self.df[["load_plant_water_chilled"]]
-        )
-
-        # 3.3 Make a prediction
-        y_cubic_pred = cubic_model.predict(x_cubic)
-
-        # 3.4 Calculate R^2 value
-        r_squared_cubic = r2_score(self.df[["load_plant_water_chilled"]], y_cubic_pred)
-
-        # select regression model based on R^2 value
-        # if the R2 value is the same, use the lower-degree model
-        max_r_squared = max(r_squared_linear, r_squared_quadratic, r_squared_cubic)
-        if r_squared_linear == r_squared_linear:
-            model = linear_model
-            pred_value = [[self.df["temperature_air_outdoor"].max()]]
-        elif r_squared_quadratic == r_squared_linear:
-            model = quadratic_model
-            pred_value = quadratic_feature.fit_transform(
-                [[self.df["temperature_air_outdoor"].max()]]
-            )
-        else:
-            model = cubic_model
-            pred_value = cubic_feature.fit_transform(
-                [[self.df["temperature_air_outdoor"].max()]]
+        acceptable_coefficient_of_determination = 0.5
+        if coefficient_of_determination >= acceptable_coefficient_of_determination:
+            outdoor_air_temperature_at_nominal_chilled_water_plant_capacity = (
+                self.calc_temp_from_load(
+                    self.df["capacity_nominal_plant_water_chilled"].iloc[0], model
+                )
             )
 
-        # Verification
-        if (self.df["temperature_air_outdoor"] > UPPER_TEMP_AIR_OUTDOOR_LIMIT).any():
-            self.df["result"] = False
-        elif (
-            model.predict(pred_value)
-            / self.df["capacity_nominal_plant_water_chilled"][0]
-            >= ACCEPTABLE_CHILLER_SIZING_RATIO
-        ):
-            self.df["result"] = True
-        else:
-            self.df["result"] = False
+            if (
+                outdoor_air_temperature_at_nominal_chilled_water_plant_capacity
+                >= UPPER_TEMP_AIR_OUTDOOR_LIMIT
+            ):
+                self.df["result"] = False
+            elif (
+                self.df["temperature_air_outdoor"].max()
+                / outdoor_air_temperature_at_nominal_chilled_water_plant_capacity
+                >= self.df["ratio_sizing_plant_water_chilled_acceptable"].iloc[0]
+            ):
+                self.df["result"] = True
+            else:
+                self.df["result"] = False
 
+        else:
+            self.df["result"] = "Untested"
         self.result = self.df["result"]
--- tests/test_chilled_water_plant_sizing_plant.py
+++ tests/test_chilled_water_plant_sizing_plant.py
@@ -1,6 +1,7 @@
 import datetime
 import sys
-import unittest
+import os
+import pytest
 
 sys.path.append("./constrain")
 import pandas as pd
@@ -8,141 +9,79 @@ from lib_unit_test_runner import *
 from library import *
 
 
-class TestChilledWaterPlantSizingWholePlant(unittest.TestCase):
-    def test_chilled_water_plant_sizing_whole_plant_missing_load_plant_water_chilled(
-        self,
-    ):
-        points = [
-            "temperature_water_supply",
-            "temperature_water_return",
-            "flow_mass_water",
-            "load_plant_water_chilled",
-            "temperature_air_outdoor",
-            "capacity_nominal_plant_water_chilled",
-        ]
-        timestamp = [
-            datetime(2023, 8, 1, 10, 0, 0),
-            datetime(2023, 8, 1, 11, 0, 0),
-            datetime(2023, 8, 1, 12, 0, 0),
-            datetime(2023, 8, 1, 13, 0, 0),
-            datetime(2023, 8, 1, 14, 0, 0),
-        ]
-        data = [
-            [7.0, 13.0, 20.0, None, 28.0, 80],
-            [7.0, 13.0, 20.0, None, 30.0, 80],
-            [7.0, 13.0, 20.0, None, 32.0, 80],
-            [7.0, 13.0, 20.0, None, 34.0, 80],
-            [7.0, 13.0, 20.0, None, 35.0, 80],
-        ]
-        df = pd.DataFrame(data, columns=points, index=timestamp)
-        expected_results = pd.Series([True, True, True, True, True])
-        verification_obj = run_test_verification_with_data(
-            "ChilledWaterPlantSizingWholePlant", df
-        )
+def test_chilled_water_plant_sizing_plant_pass():
+    # Get sample data from CSV
+    csv_path = f"{os.path.dirname(os.path.abspath(__file__))}/api/data/chilled_water_plant_sizing.csv"
+    df = pd.read_csv(csv_path)
 
-        results = pd.Series(list(verification_obj.result))
-        binaryflag = verification_obj.check_bool()
-        self.assertTrue(results.equals(expected_results))
-        self.assertTrue(binaryflag)
+    # Create datetime index
+    start_datetime = "2023-10-01 00:00"
+    periods = 525600  # One year of minute data
+    minute_index = pd.date_range(start=start_datetime, periods=periods, freq="T")
+    df.index = minute_index
 
-    def test_chilled_water_plant_sizing_whole_plant_high_temp_air_outdoor(self):
-        points = [
-            "temperature_water_supply",
-            "temperature_water_return",
-            "flow_mass_water",
-            "load_plant_water_chilled",
-            "temperature_air_outdoor",
-            "capacity_nominal_plant_water_chilled",
-        ]
-        timestamp = [
-            datetime(2023, 8, 1, 10, 0, 0),
-            datetime(2023, 8, 1, 11, 0, 0),
-            datetime(2023, 8, 1, 12, 0, 0),
-            datetime(2023, 8, 1, 13, 0, 0),
-            datetime(2023, 8, 1, 14, 0, 0),
-        ]
-        data = [
-            [7.0, 13.0, 20.0, 85.4, 50.0, 86],
-            [7.0, 13.0, 20.0, 80, 30.0, 86],
-            [7.0, 13.0, 20.0, 77.4, 32.0, 86],
-            [7.0, 13.0, 20.0, 77.6, 34.0, 86],
-            [7.0, 13.0, 20.0, 80, 35.0, 86],
-        ]
-        df = pd.DataFrame(data, columns=points, index=timestamp)
-        expected_results = pd.Series([False, False, False, False, False])
-        verification_obj = run_test_verification_with_data(
-            "ChilledWaterPlantSizingWholePlant", df
-        )
+    # Drop the Date/Time column if it exists
+    if "Date/Time" in df.columns:
+        df.drop("Date/Time", axis=1, inplace=True)
 
-        results = pd.Series(list(verification_obj.result))
-        binaryflag = verification_obj.check_bool()
-        self.assertTrue(results.equals(expected_results))
-        self.assertFalse(binaryflag)
+    # Perform verification using run_test_verification_with_data
+    verification_obj = run_test_verification_with_data(
+        "ChilledWaterPlantSizingWholePlant", df
+    )
 
-    def test_chilled_water_plant_sizing_whole_plant_linear_model(self):
-        points = [
-            "temperature_water_supply",
-            "temperature_water_return",
-            "flow_mass_water",
-            "load_plant_water_chilled",
-            "temperature_air_outdoor",
-            "capacity_nominal_plant_water_chilled",
-        ]
-        timestamp = [
-            datetime(2023, 8, 1, 10, 0, 0),
-            datetime(2023, 8, 1, 11, 0, 0),
-            datetime(2023, 8, 1, 12, 0, 0),
-            datetime(2023, 8, 1, 13, 0, 0),
-            datetime(2023, 8, 1, 14, 0, 0),
-        ]
-        data = [
-            [7.0, 13.0, 20.0, 66, 28.0, 80],
-            [7.0, 13.0, 20.0, 70, 30.0, 80],
-            [7.0, 13.0, 20.0, 74, 32.0, 80],
-            [7.0, 13.0, 20.0, 78, 34.0, 80],
-            [7.0, 13.0, 20.0, 80, 35.0, 80],
-        ]
-        df = pd.DataFrame(data, columns=points, index=timestamp)
-        expected_results = pd.Series([True, True, True, True, True])
-        verification_obj = run_test_verification_with_data(
-            "ChilledWaterPlantSizingWholePlant", df
-        )
+    # Check results
+    results = pd.Series(list(verification_obj.result))
+    assert results.all()
 
-        results = pd.Series(list(verification_obj.result))
-        binaryflag = verification_obj.check_bool()
-        self.assertTrue(results.equals(expected_results))
-        self.assertTrue(binaryflag)
 
-    def test_chilled_water_plant_sizing_whole_plant_cubic_model(self):
-        points = [
-            "temperature_water_supply",
-            "temperature_water_return",
-            "flow_mass_water",
-            "load_plant_water_chilled",
-            "temperature_air_outdoor",
-            "capacity_nominal_plant_water_chilled",
-        ]
-        timestamp = [
-            datetime(2023, 8, 1, 10, 0, 0),
-            datetime(2023, 8, 1, 11, 0, 0),
-            datetime(2023, 8, 1, 12, 0, 0),
-            datetime(2023, 8, 1, 13, 0, 0),
-            datetime(2023, 8, 1, 14, 0, 0),
-        ]
-        data = [
-            [7.0, 13.0, 20.0, 85.4, 28.0, 86],
-            [7.0, 13.0, 20.0, 80, 30.0, 86],
-            [7.0, 13.0, 20.0, 77.4, 32.0, 86],
-            [7.0, 13.0, 20.0, 77.6, 34.0, 86],
-            [7.0, 13.0, 20.0, 80, 35.0, 86],
-        ]
-        df = pd.DataFrame(data, columns=points, index=timestamp)
-        expected_results = pd.Series([False, False, False, False, False])
-        verification_obj = run_test_verification_with_data(
-            "ChilledWaterPlantSizingWholePlant", df
-        )
+def test_chilled_water_plant_sizing_plant_fail():
+    # Get sample data from CSV
+    csv_path = f"{os.path.dirname(os.path.abspath(__file__))}/api/data/chilled_water_plant_sizing.csv"
+    df = pd.read_csv(csv_path)
+    df["capacity_nominal_plant_water_chilled"] = 1812231.335 * 2 / 1.15
 
-        results = pd.Series(list(verification_obj.result))
-        binaryflag = verification_obj.check_bool()
-        self.assertTrue(results.equals(expected_results))
-        self.assertFalse(binaryflag)
+    # Create datetime index
+    start_datetime = "2023-10-01 00:00"
+    periods = 525600  # One year of minute data
+    minute_index = pd.date_range(start=start_datetime, periods=periods, freq="T")
+    df.index = minute_index
+
+    # Drop the Date/Time column if it exists
+    if "Date/Time" in df.columns:
+        df.drop("Date/Time", axis=1, inplace=True)
+
+    # Perform verification using run_test_verification_with_data
+    verification_obj = run_test_verification_with_data(
+        "ChilledWaterPlantSizingWholePlant", df
+    )
+
+    # Check results
+    results = pd.Series(list(verification_obj.result))
+    assert ~results.any()
+
+
+def test_chilled_water_plant_sizing_plant_untested():
+    # Get sample data from CSV
+    csv_path = f"{os.path.dirname(os.path.abspath(__file__))}/api/data/chilled_water_plant_sizing.csv"
+    df = pd.read_csv(csv_path)
+    df["capacity_nominal_plant_water_chilled"] = 1812231.335 * 2 / 1.15
+    df["load_plant_water_chilled"][:300000] = 0
+
+    # Create datetime index
+    start_datetime = "2023-10-01 00:00"
+    periods = 525600  # One year of minute data
+    minute_index = pd.date_range(start=start_datetime, periods=periods, freq="T")
+    df.index = minute_index
+
+    # Drop the Date/Time column if it exists
+    if "Date/Time" in df.columns:
+        df.drop("Date/Time", axis=1, inplace=True)
+
+    # Perform verification using run_test_verification_with_data
+    verification_obj = run_test_verification_with_data(
+        "ChilledWaterPlantSizingWholePlant", df
+    )
+
+    # Check results
+    results = pd.Series(list(verification_obj.result))
+    assert (results == "Untested").all()
