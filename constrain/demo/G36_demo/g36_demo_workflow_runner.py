fae471cb516dac9ac4b9085c8fa4f7121b711d2c
Xuechen (Jerry) Lei <xuechen.lei@pnnl.gov>
2025-10-24 13:50:38 -0700

    g36 test updates

--- constrain/demo/G36_demo/g36_demo_workflow_runner.py
+++ constrain/demo/G36_demo/g36_demo_workflow_runner.py
@@ -1,8 +1,21 @@
 import warnings
-import sys
-import constrain
+import sys, os
 
-from constrain.api import Workflow
+try:
+    # works in scripts
+    base_path = os.path.dirname(__file__)
+except NameError:
+    # fallback for notebooks
+    base_path = os.getcwd()
+
+# Insert the root directory at the beginning of sys.path to prioritize local constrain package
+# sys.path.insert(0, os.path.join(base_path, "..", "..", ".."))
+os.chdir(os.path.join(base_path, "..", ".."))
+print(os.getcwd())
+sys.path.insert(0, os.path.join('..'))
+import constrain
+from constrain.api import Workflow, WorkflowEngine
+
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.simplefilter(action="ignore", category=ResourceWarning)
